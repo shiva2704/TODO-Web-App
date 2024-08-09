bc5b35e78f616d61976b469efb603f6350e36742
Shiva <k.shiva27apr@gmail.com>
2024-08-09 19:29:11 +0530

    My First Angular Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Item } from './item';
+import { ItemComponent } from './item/item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo';
-}
+  componentTitle = 'My To Do List';
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+
+  addItem(description: string){
+    if(!description) return;
+
+    this.allItems.push({
+      description,
+      done: false
+    });
+  };
+
+  get items(){
+    if(this.filter == "all"){
+      return this.allItems;
+    }
+    return this.allItems.filter((item)=>
+      this.filter === "done"? item.done : !item.done
+    );
+  };
+
+  remove(item: Item){
+    this.allItems.splice(this.allItems.indexOf(item),1);
+  };
+};
